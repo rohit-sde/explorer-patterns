43c92d074255122b22dba8d03d530648ed5d9b00
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-23 23:28:57 +0530

    handling keys Dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
-import Accordion from "./components/Accordion.jsx";
+import SearchableList from "./SearchableList/SearchableList.jsx";
+import Accordion from "./components/Accordion/Accordion.jsx";
+import Place from "./Place.jsx";
+
+import savannaImg from "./assets/african-savanna.jpg";
+import amazonImg from "./assets/amazon-river.jpg";
+import caribbeanImg from "./assets/caribbean-beach.jpg";
+import desertImg from "./assets/desert-dunes.jpg";
+import forestImg from "./assets/forest-waterfall.jpg";
 
 function App() {
+    const PLACES = [
+        {
+            id: "african-savanna",
+            image: savannaImg,
+            title: "African Savanna",
+            description: "Experience the beauty of nature.",
+        },
+        {
+            id: "amazon-river",
+            image: amazonImg,
+            title: "Amazon River",
+            description: "Get to know the largest river in the world.",
+        },
+        {
+            id: "caribbean-beach",
+            image: caribbeanImg,
+            title: "Caribbean Beach",
+            description: "Enjoy the sun and the beach.",
+        },
+        {
+            id: "desert-dunes",
+            image: desertImg,
+            title: "Desert Dunes",
+            description: "Discover the desert life.",
+        },
+        {
+            id: "forest-waterfall",
+            image: forestImg,
+            title: "Forest Waterfall",
+            description: "Listen to the sound of the water.",
+        },
+    ];
     return (
         <main>
             <section>
@@ -42,6 +82,17 @@ function App() {
                         </Accordion.Content>
                     </Accordion.Item>
                 </Accordion>
+            </section>
+            <section>
+                <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
+                    {(item) => <Place item={item} />}
+                </SearchableList>
+                <SearchableList
+                    items={["item 1", "item 2"]}
+                    itemKeyFn={(item) => item}
+                >
+                    {(item) => item}
+                </SearchableList>
             </section>
         </main>
     );
