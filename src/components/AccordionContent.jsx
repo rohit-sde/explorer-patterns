ec6eff26ec91431a7ff12e44499fec5ccef7307c
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-23 22:32:23 +0530

    sharing state in cross component

--- src/components/AccordionContent.jsx
+++ src/components/AccordionContent.jsx
@@ -1,7 +1,9 @@
 import { useAccordionContext } from "./Accordion.jsx";
+import { useAccordionItemContext } from "./AccordionItem.jsx";
 
-export default function AccordionContext({ id, className, children }) {
+export default function AccordionContext({ className, children }) {
     const { openItemId } = useAccordionContext();
+    const id = useAccordionItemContext();
 
     const isOpen = openItemId === id;
 
